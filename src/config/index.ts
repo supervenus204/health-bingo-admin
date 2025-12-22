f7a6b3a4e6251b445a0e69109b872cdec1dd5fd1
supervenus204 <supervenus204@proton.me>
2025-12-22 03:52:08 -0600

    update backend url

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,3 @@
 export const API_BASE_URL =
-  // 'https://healthbingo-backend-dev-69f9daf23457.herokuapp.com';
-  'http://localhost:3000';
+  'https://healthbingo-backend-dev-69f9daf23457.herokuapp.com';
+  // 'http://localhost:3000';
